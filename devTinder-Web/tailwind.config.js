834db7f79eaf89b034007601b8b64af5a54557c9
Pankaj Patil <pankajdp23@gmail.com>
2025-09-04 11:32:47 +0530

    Hello World

---
+++ devTinder-Web/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: [
+    // your paths here, e.g. './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // other plugins if needed
+  ],
+};
