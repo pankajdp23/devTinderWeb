88ce5bb8ac7557800d86439bf9765c765fb37790
Pankaj Patil <pankajdp23@gmail.com>
2025-09-27 18:16:13 +0530

    feat: Connection Page

--- devTinder-Web/src/App.jsx
+++ devTinder-Web/src/App.jsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 import Login from "./components/Login";
 import Feed from "./components/Feed";
+import Connections from "./components/Connections";
 
 function App() {
   return (
@@ -16,8 +17,8 @@ function App() {
               <Route path="/" element={<Feed />} />
               <Route path="/login" element={<Login />} />
               <Route path="/profile" element={<Profile />} />
-              {/* <Route path="/connections" element={<Connections />} />
-              <Route path="/requests" element={<Profile />} /> */}
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Profile />} />
             </Route>
           </Routes>
         </BrowserRouter>
